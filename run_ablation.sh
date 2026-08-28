7b7967097ade0c672784fb1e14c3ec2c10f236e3
Youm1OuO <511220826@qq.com>
2026-08-28 11:09:35 +0800

    保存一下

--- run_ablation.sh
+++ run_ablation.sh
@@ -14,12 +14,13 @@ python train.py --config_file ${CONFIG_FILE} \
     MODEL.CLS_MLP_RATIO '4.0' \
     MODEL.USE_ROPE 'True' \
     MODEL.ABS_POS_MODE 'cross_attn' \
-    SOLVER.OPTIMIZER_NAME 'SGD' \
+    SOLVER.OPTIMIZER_NAME 'AdamW' \
+    SOLVER.BASE_LR '3e-5' \
+    SOLVER.WEIGHT_DECAY '1e-4' \
     SOLVER.MAX_EPOCHS '300' \
     SOLVER.EVAL_PERIOD '20' \
-    OUTPUT_DIR "./logs/Market_1501/Day1/e6_300epoch_CLS_SEP=[True+dynamic+4.0]_USE_ROPE=[True]_ABS_POS_MODE=[cross_attn]"
+    OUTPUT_DIR "./logs/Market_1501/Day1/e8_300_AdamW[3e-5+1e-4]_CLS=[True+dynamic+4.0]_ROPE=[True]_POS=[cross_attn]"
 sleep 5
-
 
 
 echo "所有消融实验全部跑完"
